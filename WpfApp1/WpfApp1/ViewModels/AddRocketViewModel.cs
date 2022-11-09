df5c3032d14f65db821ac5253478df6a4cfb74da
golovkov <79478209+golovkov-llc@users.noreply.github.com>
2022-11-09 22:29:30 +0100

    added: addRocketViewModel

---
+++ WpfApp1/WpfApp1/ViewModels/AddRocketViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp1.ViewModels
+{
+    class AddRocketViewModel : ViewModelBase
+    {
+        private string _name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        private int _engineNumber;
+        public int EngineNumber
+        {
+            get
+            {
+                return _engineNumber;
+            }
+            set
+            {
+                _engineNumber = value;
+                OnPropertyChanged(nameof(EngineNumber));
+            }
+        }
+
+        private string _engineType;
+        public string EngineType
+        {
+            get
+            {
+                return _engineType;
+            }
+            set
+            {
+                _engineType = value;
+                OnPropertyChanged(nameof(EngineType));
+            }
+        }
+
+        public ICommand AddCommand { get; }
+
+        public ICommand CancelCommand { get; }
+
+        public AddRocketViewModel()
+        {
+
+        }
+    }
+}
